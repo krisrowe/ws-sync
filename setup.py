96f2b7b12d9f315a1727f2bf935d3fc26de457de
Kris Rowe <kris.rowe@rowelink.com>
2025-11-27 14:06:08 -0600

    Update existing files to support new precommit command

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        'devws_cli': ['unsafe-patterns.yaml', 'startup.sh'],
+    },
     install_requires=[
         'Click',
         'PyYAML',
